237393806bb4ed7ad64f5623044737a0aedcf814
hvdthong <hvdthong@gmail.com>
2019-01-31 17:40:06 +0800

    add eval

--- evaluation.py
+++ evaluation.py
@@ -19,15 +19,17 @@ def eval(data, model):
         model.eval()  # since we use drop out
         all_predict, all_label = list(), list()
         for batch in data:
-            pad_msg, pad_code, labels = batch
+            pad_msg, pad_added_code, pad_removed_code, labels = batch
             if torch.cuda.is_available():
-                pad_msg, pad_code = torch.tensor(pad_msg).cuda(), torch.tensor(pad_code).cuda()
+                pad_msg, pad_added_code, pad_removed_code = torch.tensor(pad_msg).cuda(), torch.tensor(
+                    pad_added_code).cuda(), torch.tensor(pad_removed_code).cuda()
             else:
-                pad_msg, pad_code = torch.tensor(pad_msg).long(), torch.tensor(pad_code).long()
+                pad_msg, pad_added_code, pad_removed_code = torch.tensor(pad_msg).long(), torch.tensor(
+                    pad_added_code).cuda(), torch.tensor(pad_removed_code).cuda()
             if torch.cuda.is_available():
-                predict = model.forward(pad_msg, pad_code).cpu().detach().numpy().tolist()
+                predict = model.forward(pad_msg, pad_added_code, pad_removed_code).cpu().detach().numpy().tolist()
             else:
-                predict = model.forward(pad_msg, pad_code).detach().numpy().tolist()
+                predict = model.forward(pad_msg, pad_added_code, pad_removed_code).detach().numpy().tolist()
             all_predict += predict
             all_label += labels.tolist()
         acc, prc, rc, f1, auc_ = evaluation_metrics(y_pred=all_predict, y_true=all_label)
